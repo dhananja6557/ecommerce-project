b2fc00c7970eff66fcc6d95aa4ccf6bcf678f767
dhananja6557 <info.bizlanka@gmail.com>
2025-01-31 21:14:52 +0530

    commit updates

---
+++ src/pages/shop/TrendingProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrendingProducts = () => {
+    return (
+        <section className='section__container product__container'>
+            <h2 className='section__header'>Trending Products</h2>
+            <p className='section__subheader mb-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sit perspiciatis quae ex aut incidunt saepe ab aperiam facere, similique ut corrupti dolore temporibus ipsam enim. Sed odio accusamus sequi.</p>
+        </section>
+    );
+}
+
+export default TrendingProducts;
